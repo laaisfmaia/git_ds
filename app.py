a6779a5e125045ff204262e109cec42f8da0823e
Lais Maia <laais.fmaia@gmail.com>
2023-08-28 21:41:56 -0300

    utilizado função de descricao de dados

--- app.py
+++ app.py
@@ -38,7 +38,9 @@ def create_dataframe_section(df):
 def main():
     df_raw = load_data()
 
-    st.dataframe(df)
+    create_dataframe_section(df_raw)
+
+    st.dataframe(df_raw)
     
 if __name__ == '__main__':
     main()
